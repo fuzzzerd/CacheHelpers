3d80b47573ecd479421d209ba0b0c1fd69a25d88
Nate Bross <nate@brosstribe.com>
2018-01-18 21:32:52 -0600

    initial commit.

---
+++ src/CacheHelpers/JsonHelpers.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheHelpers
+{
+    public static class JsonHelpers
+    {
+        public static async Task SetAsync<T>(this IDistributedCache cache, string key, T data)
+        {
+            var jsonString = JsonConvert.SerializeObject(data);
+            var jsonBytes = Encoding.UTF8.GetBytes(jsonString);
+            await cache.SetAsync(key, jsonBytes);
+        }
+       
+        public static async Task<T> GetAsync<T>(this IDistributedCache cache, string key)
+        {
+            var jsonBytes = await cache.GetAsync(key);
+            var jsonString = Encoding.UTF8.GetString(jsonBytes);
+            return JsonConvert.DeserializeObject<T>(jsonString);
+        }
+    }
+}
